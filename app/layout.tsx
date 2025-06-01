a49b896b81ada5321ba6778c250095177d318b94
tsuji-riya <tsuji.riya@gmail.com>
2025-06-01 14:11:10 +0900

    ✨ Create Login page with form and card components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/ui/footer-section";
+import React from "react";
+import {HeroHeader} from "@/components/Home";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HeroHeader />
         {children}
         <Footer />
       </body>
