e8864c071495973c53078f3ff62c2b6631b04d03
Tsuji Riya <tsuji.riya@gmail.com>
2025-06-03 20:35:35 +0900

    Migrate to nuxtjs (#26)

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/login-form";
-
-export default function LoginPage() {
-  return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <LoginForm />
-    </div>
-  );
-}
