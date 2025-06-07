2564eae709457765efe1d44c9fec713b50c8b35e
Tsuji Riya <tsuji.riya@gmail.com>
2025-06-07 17:32:54 +0900

    refactor: simplify login button logic and remove unused script

--- components/HeaderLoginButton.vue
+++ components/HeaderLoginButton.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
 const auth = useAuth();
+const login = () => {
+  navigateTo("/login");
+};
 </script>
 
 <template>
   <div>
     <UAvatar v-if="auth.isLogin" :src="auth.user.value?.avatar" class="" />
-    <UButton
-      v-else
-      :on-click="
-        () => {
-          navigateTo('/login');
-        }
-      "
-      >ログイン</UButton
-    >
+    <UButton v-else :on-click="login">ログイン</UButton>
   </div>
 </template>
 
--- components/HeaderTitle.vue
+++ components/HeaderTitle.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <NuxtLink to="/">
     <h1 class="text-2xl font-bold">
